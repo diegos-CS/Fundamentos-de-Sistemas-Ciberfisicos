d08db1e74ed7eb4dd02cdacaa375b728f91ae0a3
Diego <diego.fobs@gmail.com>
2026-04-02 20:39:49 -0300

    Added ASCII challenge

---
+++ Desafio - ASCII.py
@@ -0,0 +1,11 @@
+# Binário
+print(chr(int("1000100", 2)) + chr(int("1101001", 2)) + chr(int("1100101", 2))  + chr(int("1100111", 2))  + chr(int("1101111", 2)))
+print(chr(int("110001", 2)) + chr(int("111001", 2)) + "/" + chr(int("110000", 2)) + chr(int("110001", 2)) + "/" + chr(int("110010", 2)) + chr(int("110000", 2)) + chr(int("110000", 2)) + chr(int("110011", 2)))
+
+# Hexadecimal
+print(chr(int("44", 16)) + chr(int("69", 16)) + chr(int("65", 16))  + chr(int("67", 16))  + chr(int("6F", 16)))
+print(chr(int("31", 16)) + chr(int("39", 16)) + "/" + chr(int("30", 16)) + chr(int("31", 16)) + "/" + chr(int("32", 16)) + chr(int("30", 16)) + chr(int("30", 16)) + chr(int("33", 16)))
+
+# Decimal
+print(chr(int("68")) + chr(int("105")) + chr(int("101"))  + chr(int("103"))  + chr(int("111")))
+print(chr(int("49")) + chr(int("57")) + "/" + chr(int("48")) + chr(int("49")) + "/" + chr(int("50")) + chr(int("48")) + chr(int("48")) + chr(int("51")))
